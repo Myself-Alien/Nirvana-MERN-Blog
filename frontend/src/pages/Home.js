09d93faebb9ce9c441c8303209d20875a5bda3cb
Myself-Alien <pritimaypanda1993@gmail.com>
2025-03-11 02:47:47 +0530

    fixing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -62,64 +62,47 @@ const Home = () => {
           {/* 📌 Blog List */}
           {filteredBlogs.length > 0 ? (
             filteredBlogs.map((blog) => (
-              <div
-                className="row pt-4 pb-2 p-3"
-                key={blog._id}
-                style={{ display: "flex", alignItems: "stretch" }}
-              >
-                {/* Blog Image */}
-                <div className="col-md-4">
-                  <Link to={`/blog/${blog.slug}`}>
-                    {blog.image && (
-                      <img
-                        src={`http://localhost:5000${blog.image}`}
-                        alt={blog.title}
-                        className="blog-thumbnail"
-                        loading="lazy"
-                        style={{ maxWidth: "100%", height: "auto" }}
-                      />
-                    )}
-                  </Link>
-                </div>
+              <div className="row pt-4 pb-2 p-3" key={blog._id} style={{ display: "flex", alignItems: "stretch" }}>
+  {/* Blog Image */}
+  <div className="col-md-4">
+    <Link to={`/blog/${blog.slug}`}>
+      {blog.image && (
+        <img
+          src={`http://localhost:5000${blog.image}`}
+          alt={blog.title}
+          className="blog-thumbnail"
+          loading="lazy"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+      )}
+    </Link>
+  </div>
 
-                {/* Blog Content - Fixed Height & Button at Bottom */}
-                <div
-                  className="col-md-8 d-flex flex-column"
-                  style={{ height: "100%" }}
-                >
-                  <div style={{ flexGrow: 1 }}>
-                    <h2 className="blog-title">
-                      <Link to={`/blog/${blog.slug}`} className="text-dark">
-                        {blog.title}
-                      </Link>
-                    </h2>
-                    <p>
-                      <strong>Posted on:</strong>{" "}
-                      {new Date(blog.createdAt).toLocaleString()}
-                    </p>
-                    <p>
-                      <strong>Author:</strong> {blog.author}
-                    </p>
+  {/* Blog Content - Fixed Height & Button at Bottom */}
+  <div className="col-md-8 d-flex flex-column" style={{ height: "100%" }}>
+    <div style={{ flexGrow: 1 }}>
+      <h2 className="blog-title">
+        <Link to={`/blog/${blog.slug}`} className="text-dark">
+          {blog.title}
+        </Link>
+      </h2>
+      <p><strong>Posted on:</strong> {new Date(blog.createdAt).toLocaleString()}</p>
+      <p><strong>Author:</strong> {blog.author}</p>
 
-                    {/* Force Description to be max 3 lines */}
-                    <p className="blog-description">
-                      {blog.description
-                        ? blog.description.substring(0, 150) + "..."
-                        : "No description available"}
-                    </p>
-                  </div>
-                  {/* "Read More" Button Always at Bottom */}
-                  <div>
-                    <Link
-                      to={`/blog/${blog.slug}`}
-                      className="read-more btn btn-primary"
-                    >
-                      Read More
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))
+      {/* Force Description to be max 3 lines */}
+      <p className="blog-description">
+        {blog.description ? blog.description.substring(0, 150) + "..." : "No description available"}
+      </p>
+    </div>
+    {/* "Read More" Button Always at Bottom */}
+    <div>
+      <Link to={`/blog/${blog.slug}`} className="read-more btn btn-primary">
+        Read More
+      </Link>
+    </div>
+  </div>
+</div>
+))
           ) : (
             <h3 className="text-center mt-4">No blogs found</h3>
           )}
